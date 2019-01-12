a03ef2fb9237b866375185a357438ee32b232d5a
Kay Hau <virtualda@gmail.com>
2019-01-13 10:32:47 +1100

    Implemented some exercises

---
+++ short/util.py
@@ -0,0 +1,12 @@
+
+# Convert decimal to binary with bin(), which returns result with prefix `0b`.
+decimal_to_binary = lambda x : int(bin(x)[2:])
+
+# Check if the given number is power of 2.
+is_power_of_2 = lambda x : (x > 0) and ((x & (x-1)) == 0)
+
+
+# Generate array of N integers; N is within the range [1..100,000];
+# Each element of array A is an integer within the range [-1,000,000..1,000,000].
+import random
+random_A = [random.randint(-100000, 1000000) for n in range(random.randint(1, 100000))]
