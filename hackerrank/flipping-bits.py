3ed35b7a54db60f685697716a88ebc8c09ec3a75
Kay Hau <virtualda@gmail.com>
2019-01-23 19:35:07 +1100

    Implemented some exercises

---
+++ hackerrank/flipping-bits.py
@@ -0,0 +1,24 @@
+"""
+https://www.hackerrank.com/challenges/flipping-bits/problem
+"""
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the flippingBits function below.
+def flippingBits(n):
+
+    #def decimal_to_binary(n):
+    #    return bin(n)[2:]
+
+    #def binary_to_decimal(n):
+    #    return int(n,2)
+
+    #b = decimal_to_binary(n)
+    #b = f'{b:0>{32}}'
+
+    # Simpler
+    return n ^ 0xffffffff
+
