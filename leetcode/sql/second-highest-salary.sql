70471f7299e329b6de90d28c2d2c4c1b6d1d8f08
Kay Hau <virtualda@gmail.com>
2019-02-14 20:14:02 +1100

    Implemented some small challenges

---
+++ leetcode/sql/second-highest-salary.sql
@@ -0,0 +1,9 @@
+/* https://leetcode.com/problems/second-highest-salary/
+ */
+
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary NOT IN (
+    SELECT MAX(Salary)
+    FROM Employee
+);
